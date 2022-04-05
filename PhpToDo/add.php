4b444a6054ae543bd41c18612aa5fa2d0dbf307d
Tim Force <twforce@yahoo.com>
2022-04-05 14:26:42 -0700

    get recently added item id

--- PhpToDo/add.php
+++ PhpToDo/add.php
@@ -11,25 +11,28 @@
             print("Adding: " . $newMessage);
             print("<br /><br />");
             
-    $mysqli = mysqli_connect("localhost", "root", "", "productivity_mine");
+    $mysqli_conn = mysqli_connect("localhost", "root", "", "productivity_mine");
     if (mysqli_connect_errno()) {
       printf("Connect failed: %s\n", mysqli_connect_errno());
       exit();
     } else {
-      $clean_msg = mysqli_real_escape_string($mysqli, $newMessage);
-      $clean_priority = mysqli_real_escape_string($mysqli, $newPriority);
+      $clean_msg = mysqli_real_escape_string($mysqli_conn, $newMessage);
+      $clean_priority = mysqli_real_escape_string($mysqli_conn, $newPriority);
       $sql = "insert into to_do_items (message, priority, created_at) values ('" . $clean_msg . "', '" . $clean_priority . "', now())";
-      $res = mysqli_query($mysqli, $sql);
+      $res = mysqli_query($mysqli_conn, $sql);
+
+      $insertId = $mysqli_conn->insert_id;
+      echo "<input type='hidden' name='insertId' value='" . $insertId . "' />";
 
       if ($res === TRUE) {
           echo "A new record has been added";
       } else {
-        printf("Could not add record: %s\n", mysqli_error($mysqli));
+        printf("Could not add record: %s\n", mysqli_error($mysqli_conn));
       }
     }
 
     // mysqli_free_result($res);
-    mysqli_close($mysqli);
+    mysqli_close($mysqli_conn);
         ?>
 
         <section>
