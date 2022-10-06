210599ec3e6859debbb53e7877217900cf20219a
Tim F <twforce@yahoo.com>
2022-10-05 20:31:35 -0700

    starting to do list version with create react app

---
+++ ReactToDo/CreateReactApp/to-do-list-app/src/ToDoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./ToDoList.css";
+
+function ToDoList() {
+    return (
+        <div id="comp-to-do-list">
+            <h2>To Do List</h2>
+        </div>
+    );
+}
+
+export default ToDoList;
